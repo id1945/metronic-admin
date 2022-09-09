f0671e8c4d733d5a181c9f81c73b4dbbae63a742
Demilade <53810496+Demitoye@users.noreply.github.com>
2022-09-09 03:27:47 +0100

    Delete theme/html_laravel/demo1/skeleton directory

--- theme/html_laravel/demo1/skeleton/resources/metronic/plugins/formvalidation/dist/es6/validators/vat/isVat.js
+++
@@ -1,1 +0,0 @@
-export default function t(t){let e=t;if(/^IS[0-9]{5,6}$/.test(e)){e=e.substr(2)}return{meta:{},valid:/^[0-9]{5,6}$/.test(e)}}
--- theme/html_laravel/demo1/skeleton/resources/plugins/custom/tinymce/tinymce.js
+++
@@ -1,5 +0,0 @@
-// TinyMCE: The rich text editor built to scale, designed to innovate, and developed in open source: https://www.tiny.cloud/
-
-require('tinymce/tinymce.min.js');
-require('tinymce/themes/silver/theme.js');
-require('tinymce/themes/mobile/theme.js');
